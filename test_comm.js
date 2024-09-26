7ef87a60e410c9a7b0e6a164eaf917ca9a5c3203
Laszlo Doriat <laszlo-marlon.doriat@frickly.systems>
2024-09-26 17:53:33 +0200

    new commitxv

--- test_comm.js
+++ test_comm.js
@@ -1,13 +1,21 @@
+// Utility function to display messages in the HTML
+function displayMessage(message) {
+    const messageElement = document.getElementById('message');
+    messageElement.innerText += message + '\n'; // Append the new message
+}
+
 // Sends a message to Flutter via FlutterChannel
 function sendMessageToFlutter() {
     if (typeof FlutterChannel !== 'undefined') {
         FlutterChannel.postMessage('Hello from JavaScript!');
+        displayMessage('Message sent to Flutter: Hello from JavaScript!');
     } else {
-        console.error('FlutterChannel is not defined.');
+        displayMessage('Error: FlutterChannel is not defined.');
     }
 }
 
-// Receives a message from Flutter and logs it to the console
+// Receives a message from Flutter and logs it to the console and the page
 function receiveMessageFromFlutter(message) {
     console.log('Received message from Flutter:', message);
+    displayMessage('Received message from Flutter: ' + message);
 }
