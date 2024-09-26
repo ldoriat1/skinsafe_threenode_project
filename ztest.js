6cede3af8e4c15bf177ed451fb00ed0c6c4c3d8d
Laszlo Doriat <laszlo-marlon.doriat@frickly.systems>
2024-09-26 18:16:35 +0200

    new commitxv

--- ztest.js
+++ ztest.js
@@ -321,42 +321,59 @@ function animate() {
     stats.update();
 }
 
-function mapDecalsToModel(decalsData) {
-    const decalsFromFlutter = JSON.parse(decalsData);
+// Function to display messages in the HTML
+// function displayMessage(message) {
+//     const messageElement = document.getElementById('message');
+//     messageElement.innerText += message + '\n'; // Append the new message
+// }
 
-    decalsFromFlutter.forEach(decal => {
-        const position = new THREE.Vector3(decal.position.x, decal.position.y, decal.position.z);
-        const orientation = new THREE.Euler(decal.orientation.x, decal.orientation.y, decal.orientation.z);
-        const size = new THREE.Vector3(decal.size.width, decal.size.height, decal.size.depth);
+// Function that JavaScript will listen for from Flutter to receive decals
+function receiveMessageFromFlutter(message) {
+    try {
+        // Parse the decals data received from Flutter
+        const decalsFromFlutter = JSON.parse(message);
 
-        const material = new THREE.MeshPhongMaterial({
-            color: 0x000000, // All decals from the app are black
-            specular: 0x444444,
-            shininess: 30,
-            transparent: true,
-            depthTest: true,
-            depthWrite: false,
-            polygonOffset: true,
-            polygonOffsetFactor: -4,
-            wireframe: false
+        decalsFromFlutter.forEach(decal => {
+            const position = new THREE.Vector3(decal.position.x, decal.position.y, decal.position.z);
+            const orientation = new THREE.Euler(decal.orientation.x, decal.orientation.y, decal.orientation.z);
+            const size = new THREE.Vector3(decal.size.width, decal.size.height, decal.size.depth);
+
+            const material = new THREE.MeshPhongMaterial({
+                color: 0x000000, // All decals from Flutter are black
+                specular: 0x444444,
+                shininess: 30,
+                transparent: true,
+                depthTest: true,
+                depthWrite: false,
+                polygonOffset: true,
+                polygonOffsetFactor: -4,
+                wireframe: false
+            });
+
+            const decalMesh = new THREE.Mesh(new THREE.DecalGeometry(mesh, position, orientation, size), material);
+            scene.add(decalMesh);
+
+            // Store the newly added decal in the global array
+            decals.push({
+                mesh: decalMesh,
+                position: position,
+                orientation: orientation,
+                size: size,
+                color: 0x000000 // Decal color set to black
+            });
         });
 
-        const decalMesh = new THREE.Mesh(new THREE.DecalGeometry(mesh, position, orientation, size), material);
-        scene.add(decalMesh);
+        displayMessage('Decals received and added to the scene from Flutter.');
 
-        // Store the newly added decal in the global array
-        decals.push({
-            mesh: decalMesh,
-            position: position,
-            orientation: orientation,
-            size: size,
-            color: 0x000000 // Decal color set to black
-        });
-    });
+    } catch (error) {
+        console.error('Error processing decals from Flutter:', error);
+        displayMessage('Error processing decals from Flutter: ' + error);
+    }
 }
 
-// Ensure this function is declared in the global scope
+// Function to send all decals to Flutter as JSON
 function sendAllDecalsToFlutter() {
+    // Prepare the decals data to send to Flutter
     const decalsData = decals.map(decal => ({
         position: {
             x: decal.mesh.position.x,
@@ -376,10 +393,12 @@ function sendAllDecalsToFlutter() {
         color: 0x000000 // Black color for all decals
     }));
 
-    // Ensure that DecalChannel exists and send decals data to Flutter
-    if (typeof DecalChannel !== 'undefined') {
-        DecalChannel.postMessage(JSON.stringify(decalsData));
+    // Ensure that DecalChannel exists and send the decals data to Flutter
+    if (typeof FlutterChannel !== 'undefined') {
+        FlutterChannel.postMessage(JSON.stringify(decalsData));
+        displayMessage('All decals sent to Flutter.');
     } else {
-        console.error('DecalChannel is not defined.');
+        console.error('Error: FlutterChannel is not defined.');
+        displayMessage('Error: FlutterChannel is not defined.');
     }
 }
